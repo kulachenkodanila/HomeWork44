443bbaf77ab3b23235558636382ea68d3cb67b6f
Kulachenko_Danila <kulachenkodanila@gmail.com>
2022-06-21 20:16:58 +0300

    add some code frome 36 home_work

--- HomeWork/urls.py
+++ HomeWork/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from webapp.views import home_work
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_work),
+
 
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home_work(request):
+    return render(request, "home.html")
+
+
+secret_num = ['1', '2', '3', '4']
+
+
+def checking(self):
+    if len(self.user_numbers) > 4 or len(self.user_numbers) < 4:
+        return 'Wrong number of values'
+    elif len(set(self.user_numbers)) < 4:
+        return 'Values must not be repeated'
+    try:
+        for i in range(len(self.user_numbers)):
+            if int(self.user_numbers[i]) < 1 or int(self.user_numbers[i]) > 9:
+                return 'Enter values from 1 to 9'
+    except ValueError:
+        return "Enter only numbers"
+
+
+def result_game(self):
+    count_bulls = 0
+    count_cows = 0
+    for i in range(len(self.user_numbers)):
+        if self.user_numbers[i] == self.secret_num[i]:
+            count_bulls += 1
+        elif self.user_numbers[i] in self.secret_num:
+            count_cows += 1
+    if count_bulls == 4:
+        return 'Your Win!'
+    elif count_bulls or count_cows:
+        return f'You got {count_bulls} bulls and {count_cows} cows'
+    else:
+        'No identical values'
